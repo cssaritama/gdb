84342a4948e55969ebfcce4d3277bb12462090bf
Pedro Alves <pedro@codesourcery.com>
2011-12-05 21:40:08 +0000

    Comment out new info breakpoints output, in order to not break the test suite

--- gdb/breakpoint.c
+++ gdb/breakpoint.c
@@ -4977,6 +4977,7 @@ print_one_breakpoint_location (struct breakpoint *b,
       ui_out_text (uiout, "\n");
     }
 
+#if 0 // disabled in order to be able to run the testsuite
   if (!part_of_multiple && b->trigger_set != NULL)
     {
       ui_out_text (uiout, "\tstop only in trigger-set: [");
@@ -4996,6 +4997,7 @@ print_one_breakpoint_location (struct breakpoint *b,
 	ui_out_field_string (uiout, "stop-set", itset_spec (b->stop_set));
       ui_out_text (uiout, "]\n");
     }
+#endif
 
   if (!part_of_multiple && b->hit_count)
     {
