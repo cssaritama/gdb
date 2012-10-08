321b5e98590bc58d956e8f3371b21ff6ff7cc7f5
Pedro Alves <palves@redhat.com>
2012-10-08 18:03:25 +0100

    The logic of when to insert or remove all or just the single-step breakpoints needs to be redone (IOW, it's broken...).  Here's another attempt.  Keep a global flag.

--- gdb/breakpoint.c
+++ gdb/breakpoint.c
@@ -1982,6 +1982,7 @@ in which its expression is valid.\n"),
     select_frame (frame_find_by_id (saved_frame_id));
 }
 
+static int breakpoints_removed = 0;
 
 /* Returns 1 iff breakpoint location should be
    inserted in the inferior.  We don't differentiate the type of BL's owner
@@ -2001,6 +2002,9 @@ should_be_inserted (struct bp_location *bl)
     return 0;
 
   if (user_breakpoint_p (bl->owner) && bl->pspace->executing_startup)
+    return 0;
+
+  if (bl->owner->type != bp_single_step && breakpoints_removed)
     return 0;
 
   /* This is set for example, when we're attached to the parent of a
@@ -2715,6 +2719,8 @@ insert_breakpoints (void)
 {
   struct breakpoint *bpt;
 
+  breakpoints_removed = 0;
+
   ALL_BREAKPOINTS (bpt)
     if (is_hardware_watchpoint (bpt))
       {
@@ -2941,6 +2947,8 @@ remove_breakpoints (void)
 {
   struct bp_location *bl, **blp_tmp;
   int val = 0;
+
+  breakpoints_removed = 1;
 
   ALL_BP_LOCATIONS (bl, blp_tmp)
   {
