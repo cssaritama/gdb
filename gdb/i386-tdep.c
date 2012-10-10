16e01e3f0c6cbf67d0e03b6006bc758039f949a1
Pedro Alves <palves@redhat.com>
2012-10-10 19:21:24 +0100

    Displaced stepping on x86 over relative branches/jumps as is doesn't work nicelly with SSS.  Currently, we copy the branch/jump to the jump pad, single-step it, and then adjust the relative offset after the fact.  The problem is that with SSS, the branch/jump destination relative to the scratch pad could well be an innacessible address, one where we can't put a SSS bkpt at.
    So rework the relative branch/jump support to be like ARM's.  IOW,
    compute the branch destination, have the inferior execute a nop (so it
    still has a chance to dequeue signals, etc.), SSS single-step that
    (which will always land in the scratch pad), and then simply adjust
    the PC manually to the original branch destination.

--- gdb/i386-tdep.c
+++ gdb/i386-tdep.c
@@ -7536,7 +7536,7 @@ get_sp (struct frame_info *frame)
 /* If the instruction at PC is a jump, return the address of its
    target.  Otherwise, return PC.  */
 
-static int
+int
 i386_cond_jump_dest (struct frame_info *frame,
 		     const gdb_byte *insn, const CORE_ADDR pc, CORE_ADDR *dest)
 {
@@ -7749,7 +7749,7 @@ arch_reg_to_regnum (struct gdbarch *gdbarch, int reg)
 /* If the instruction at PC is a jump, write to *DEST the address of
    its target and return true.  Otherwise, return false.  */
 
-static int
+int
 i386_jump_dest (struct frame_info *frame,
 		const gdb_byte *insn, const CORE_ADDR pc, CORE_ADDR *dest)
 {
